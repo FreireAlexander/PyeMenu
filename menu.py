8e3c967d173343aa7fd7da4110326ca1d302e4c0
Koro <freirealexander0214@gmail.com>
2023-03-22 10:47:10 -0500

    Adding clear function

--- menu.py
+++ menu.py
@@ -2,14 +2,24 @@
 import os
 import readchar
 
+# Os.clear functions
+
+def clear():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+# Menu prompt functions
 def menu():
     print("Imprimiendo menu")
+    clear()
     print(f"Viendo orden de impresión de lineas \n Hola soy Zeustaquiones")
     return
 
 # Function main for testing porpuses
 def main():
-    menu.menu()
+    menu()
 
 if __name__ == "__main__":
     main()
